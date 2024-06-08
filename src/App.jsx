24db1257bd1dcb861b93a236a872e228e1a3ae1c
tatsuki-iketsu <tatsuki.iketsu@gmail.com>
2024-06-08 17:50:20 +0900

    usestateサンプル

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import React from "react"
+import React,{useState} from "react"
 import ColorfulMessage from "./components/ColorfulMessage"
 import PropsMessage from "./components/ColorfulMessage copy";
 const App =() =>{
+    const [num,setNum] = useState(0);
+    const [textShowFlug,setTextShowFlag] = useState(true); 
+
     const onClickButten1 = ()=> alert('正解です');
     const onClickButten2 = ()=> alert('残念！不正解です');
     const onClickButten3 = ()=> alert('残念！不正解です');
-    // const contentstyle = {
-    //     color :'green' ,
-    //     fontSize : '40px'
-    // };
-    // const contentRedStyel = {
-    //     color :'green' ,
-    //     fontSize : '40px'
-    // }
+    const onClickCountUp = ()=> setNum(num + 10) ;
+    const onClickshowFlugCjange = ()=> setTextShowFlag(!textShowFlug) ;
     return (
         <>
                 <h1 style={{color:'pink' }}>問題</h1>
@@ -21,6 +18,10 @@ const App =() =>{
                 <button onClick={onClickButten1}>回答1</button>
                 <button onClick={onClickButten2}>回答2</button>
                 <button onClick={onClickButten3}>回答3</button>
+                <button onClick={onClickCountUp}>加算</button>
+                <button onClick={onClickshowFlugCjange}>表示非表示</button>
+                <p>{num}</p>
+                {textShowFlug && <p>表示されています</p>}
                </>
         );
 };
