5d7b656562c8d2d56cdcfc45276d9c183e5d649a
tatsuki-iketsu <tatsuki.iketsu@gmail.com>
2024-06-25 23:10:02 +0900

    シャッフル作成中

--- src/components/ExamSet.jsx
+++ src/components/ExamSet.jsx
@@ -4,23 +4,33 @@ import { useState } from 'react';
 // import { useEffect } from 'react';
 
 const NO_SELECTED = "回答が選択されていません"
+// const [examData,setExamData] = useState(Data);
 
 const ExamDisplay = () =>{
-  const [ansDisplay,setAnsDisplay] = useState("選択してボタンを押してください");
-  const [trueOrFalseDisplay,settrueOrFalseDisplay] = useState("ここに回答が表示されます");
-  // const [examData,setExamData] = useState(Data);
+  const [ansDisplay,setAnsDisplay] = useState(["選択してボタンを押してください1","選択してボタンを押してください2","3","4","5"]);
+  const [trueOrFalseDisplay,settrueOrFalseDisplay] = useState(["ここに回答が表示されます1","ここに回答が表示されます2","3","4","5"]);
 
+  
 // シャッフル
-const abc = ["A","B","C","D","E","F","G"];
-const shuffleExam=(selectAnswers)=> {
-    for (let i = selectAnswers.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [selectAnswers[i], selectAnswers[j]] = [selectAnswers[j], selectAnswers[i]];
+// シャッフル
+const shuffleExam =(selectAnswers)=> {
+    for (let ia = selectAnswers.length - 1; ia > 0; ia--) {
+      const jb = Math.floor(Math.random() * (ia + 1));
+      [selectAnswers[ia], selectAnswers[jb]] = [selectAnswers[jb], selectAnswers[ia]];
     }
     selectAnswers.map((selectAnswer,i)=>{
       selectAnswer.item = abc[i];
     })
     return selectAnswers;
+  }
+
+const abc = ["A","B","C","D","E","F","G"];
+const shuffleExam1=(examData)=> {
+    for (let i = examData.setExams.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [examData.setExams[i], examData.setExams[j]] = [examData.setExams[j], examData.setExams[i]];
+    }
+    return examData;
   }
   // 問題と回答のセット
     const examData = Data;
@@ -54,7 +64,9 @@ const shuffleExam=(selectAnswers)=> {
     };
 
     // 複数回答の表示
-    const selectedAnserGet = (selectedChecks,setExam) => {
+    const selectedAnserGet = (selectedChecks,setExam,index) => {
+      alert(`index：${index}`);
+
       // 選択した回答を格納する配列
       const selectedAns = []
       // 正しい回答を格納する配列
@@ -76,15 +88,16 @@ const shuffleExam=(selectAnswers)=> {
                 }
               } );
         
-              setAnsDisplay(`正しい回答：${answers}/選択した回答：${selectedAns}`);
+              setAnsDisplay(ansDisplay.map((ans, i) => (i === index ? `正しい回答：${answers}/選択した回答：${selectedAns}` : ans)));
               if(answers.toString() == selectedAns.toString()){
-                settrueOrFalseDisplay("正解！！");
+                settrueOrFalseDisplay(trueOrFalseDisplay.map((trueOrFalse, i) => (i == index ? "正解！！" : trueOrFalse)));
                 // alert(`正しい回答：${answers}`);
                 // alert(`選択した回答：${selectedAns.toString()}`) ;
                 // alert(`正解！`);
                 }else{
-                  settrueOrFalseDisplay("残念…");
-                  // alert(`正しい回答：${answers}`);
+                  settrueOrFalseDisplay(trueOrFalseDisplay.map((trueOrFalse, i) => (i == index ? "残念…" : trueOrFalse)));
+                  alert(`index：${index}`);
+                  alert(`確認：${trueOrFalseDisplay}`);
                   // alert(`選択した回答：${selectedAns.toString()}`) ;
                   // alert(`誤り！`);
               }
@@ -92,11 +105,11 @@ const shuffleExam=(selectAnswers)=> {
     };
 
     // 複数選択ボタン押下時アクション
-    const selectedCheck = (setExam,name)=> {    
+    const selectedCheck = (setExam,name,index)=> {    
         let checks = document.querySelectorAll(
             `input[name=${name}]:checked`
           );
-          selectedAnserGet(checks,setExam);
+          selectedAnserGet(checks,setExam,index);
           // // 回答取得確認用
           // alert(`${correctAnswerSetCreate(setExam)}`);
     };
@@ -110,7 +123,7 @@ const shuffleExam=(selectAnswers)=> {
     <p>ここに問題文を記述します。</p>
     {/* <input type="button" onclick={shuffleExam(examData.setExams[0].selectAnswers)} > */}
     {/* <input type="button" onClick={shuffleExam(examData.setExams[0].selectAnswers)} value="リセット" /> */}
-    <input type="button" onClick={() => shuffleExam(examData.setExams[0].selectAnswers)} value="リセット" />
+    <input type="button" onClick={() => shuffleExam1(examData)} value="リセット" />
 
     {/* <input type="button" onClick={()=>shuffleExam(examData.setExams[0].selectAnswers)}>リセット</input> */}
     {/* <div>
@@ -119,7 +132,7 @@ const shuffleExam=(selectAnswers)=> {
     <button onClick={()=>onClickAnser(examData.setExams[0].selectAnswers[0])}>回答を確認する</button>
   </div> */}
 
-  {examData.setExams.map((setExam) => (
+  {examData.setExams.map((setExam,index) => (
     <>
   <div>
   <form name= {setExam.setItem}>
@@ -130,19 +143,19 @@ const shuffleExam=(selectAnswers)=> {
             <input
               type="checkbox"
               name = {setExam.setItem}
-              id = {Answer.item}
+              id = {setExam.setItem + Answer.item}
               value ={Answer.item}
             />
-            <label htmlFor={Answer.item}>{Answer.item}：{Answer.content}</label>
+            <label htmlFor={setExam.setItem + Answer.item}>{Answer.item}：{Answer.content}</label>
           </p>
         </>
       ))}
         
         </form>
-        <button onClick={()=>selectedCheck(setExam,setExam.setItem)}>確認</button>
+        <button onClick={()=>selectedCheck(setExam,setExam.setItem,index)}>確認</button>
       </div>
-      <div>{ansDisplay}</div>
-      <div>{trueOrFalseDisplay}</div>
+      <div>{ansDisplay[index]}</div>
+      <div>{trueOrFalseDisplay[index]}</div>
       </>
     ))}
       
