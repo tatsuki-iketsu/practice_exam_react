d68e95dde7dc7f0783b50993e116881c6a47ec71
池津たつき <tatsuki.iketsu@gmail.com>
2025-04-27 23:10:05 +0900

    正誤率をボタンで実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
-import React,{useState} from "react"
+import React, { useState, useEffect } from "react";
 import ExamDisplay from "./components/ExamDisplay";
 import Fotter from "./Fotter";
 import Header from "./Header";
 
 
-
 const App =() =>{
-    const [num,setNum] = useState(0);
-    const [textShowFlug,setTextShowFlag] = useState(true); 
+
+    const [totalCount,setTotalCount] = useState(39);    // 合計数
+    const [trueCount,setTrueCount] = useState(0);           // 正解数
+    const [falseCount,setFalseCount] = useState(0);         // 不正解数
+
+    console.log(`App描画/問題数:${totalCount}-/正解数::${trueCount}-/不正解数::${falseCount}`);
+
+    // // テスト関数
+    // const countUpdate =(number) =>{
+    //     console.log(`正答率リセット:${totalCount}`);
+    //     setTotalCount(inTotal);
+    //     setRestCount(inRest);
+    //     setTrueCount(inTrue);
+    //     setFalseCount(inFalse);
+    // }
+
+    // 正答率更新
+    const countUpdate =(inTotal,inTrue,inFalse) =>{
+        console.log(`正答率リセット:${totalCount}`);
+        setTotalCount(inTotal);
+        setTrueCount(inTrue);
+        setFalseCount(inFalse);
+    }
+    // 正答率更新
+    const countReset =(inTotal) =>{
+        countUpdate(inTotal,0,0)
+    }
+
     return (
-        <>    
-            <header><Header /></header>
+        <>   
+            {/* <input type="button" onClick={() => countUpdate(1)} value="テスト関数" />  */}
+            <header><Header totalCount={totalCount} trueCount={trueCount} falseCount={falseCount} countUpdate={countUpdate} countReset={countReset}/></header>
+            <input type="button" onClick={() => countUpdate(totalCount,trueCount + 1,falseCount)} value="正解追加" />
+            <input type="button" onClick={() => countUpdate(totalCount,trueCount,falseCount + 1)} value="誤り追加" />
+            <input type="button" onClick={() => countReset(totalCount)} value="リセット" />
             <main><ExamDisplay /></main>
             <Fotter />
         </>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,95 @@
 import React,{useState} from "react"
 
-const Header =() =>{
+const Header =(props) =>{
+    const [totalCount,setTotalCount] = useState(0);                          // 合計数
+    const [restCount,setRestCount] = useState(0);                          // 残問題数
+    const [trueCount,setTrueCount] = useState(0);                          // 正解数
+    const [falseCount,setFalseCount] = useState(0);                        // 不正解数
+    const [answerRate,setAnswerRate] = useState(`-`);                    // 正解率
+
+    // // 正解率の更新
+    // const countUpdate = (inTotal,inRest,inTrue,inFalse) =>{
+    //     setTotalCount(inTotal);
+    //     setRestCount(inTotal);
+    //     setTrueCount(inTrue);
+    //     setFalseCount(inFalse);
+    // }
+    alert( `Header` );
+
+    // App.jsに問題数を渡す関数
+    const countReset = (inTotal,inTrue,inFalse) => {
+        props.countReset(inTotal);
+    };
+
+
+    // 問題数のいずれかが変更されると再計算
+    React.useEffect(() => {
+        // 正解、誤りともに0なら初期設定
+        if(props.trueCount + props.falseCount == 0){
+            setRestCount(props.totalCount);
+            setAnswerRate(`-`);
+        } else{
+            // const sum ={allTrue/(allTrue+allFalse)*100}
+            console.log( `Header問題数更新:`);
+            console.log( {props});
+            const sum = (Math.round(props.trueCount / (props.trueCount + props.falseCount)*10000)) / 100;
+            alert( `sum:${sum}` );
+            setAnswerRate(`${sum}%`);
+            const rest = (props.totalCount - (props.trueCount + props.falseCount));
+            alert( `rest:` );
+            console.log( {rest});
+            setRestCount(rest);
+        }
+
+        setTotalCount(props.totalCount);
+        setTrueCount(props.trueCount);
+        setFalseCount(props.falseCount);
+    }, [props]);
+    
+    // React.useEffect(() => {
+    //     setTotalCount(props.totalCount);
+    //     setRestCount(props.totalCount);
+    //     setTrueCount(props.trueCount);
+    //     setFalseCount(props.falseCount);
+    // }, []);
+    
+
+    
+    // React.useEffect(() => {
+    //         setTotalCount(props.totalCount);
+    //         setRestCount(props.restCount);
+    //         setTrueCount(props.trueCount);
+    //         setFalseCount(props.totalFalse);
+    //     }, []);
+    
+    // const firstSet = (inNumber) =>{
+    //     setallTrue(allTrue + inNumber);
+    // }
+    // <input type="button" onClick={() => shuffleExam1(examData,false)} value="リセット" />
+    React.useEffect(() => {
+        // console.log('Hello world!');
+        // firstSet(props.number);
+        // console.log(`props:${props}1`);
+        // console.log(`props.allExam:${props.allExam}1`);
+        // setallExam(props.totalCount);
+        // setallTrue(props.trueCount);
+        // setallFalse(props.falseCount);
+        // if(!(props.trueCount + props.falseCount == 0)){
+        //     // const sum ={allTrue/(allTrue+allFalse)*100}
+        //     alert( `trueCount:${trueCount}` );
+        //     let sum = (allTrue/(allTrue+allFalse))*100;
+        //     // let sum = (props.falseTrue/(props.falseTrue+props.falseCount))*100;
+        //     alert( `sum:${sum}` );
+        //     setAnswerRate(`${sum}%`);
+        // }
+        // alert( `props.allExam:${allExam}` );
+    }, []);
+
     return (
         <>  
         <h1 id="h1">Java SE silver17 練習問題</h1>
+        <p id="bottom">残問題数：{restCount}/{totalCount}　正解数：{trueCount}　不正解数：{falseCount}　正解率：{answerRate}</p>
+        <input type="button" onClick={() => countReset(totalCount)} value="正答率リセット" />
         <div> </div>
         </>
         );
