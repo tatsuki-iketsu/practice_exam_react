293bbb83fc39f620803989fbec8ea0ee5f332107
池津たつき <shinji_ikeda@ollakunnossa.com>
2025-05-06 19:31:57 +0900

    問題追加中

--- src/UrlList.jsx
+++ src/UrlList.jsx
@@ -1,12 +1,14 @@
 import CompileAnrRun from "./components/explanationPage/CompileAnrRun";
 import Collection from "./components/explanationPage/Collection";
 import OverrideOfMethod from "./components/explanationPage/OverrideOfMethod";
+import ClassAndInstance from "./components/explanationPage/ClassAndInstance";
 
   const ExplanationData =() =>{
     const explanationData = [
         { pageID: 1, url: `/CompileAnrRun` ,component:<CompileAnrRun />,pageName:"コンパイル、実行"},
-        { pageID: 2, url: `/Collection` ,component:<Collection />,pageName:"コレクション"},
-        { pageID: 3, url: `/Overrideofmethod` ,component:<OverrideOfMethod />,pageName:"メソッドのオーバーライド"},
+        { pageID: 2, url: `/ClassAndInstance` ,component:<ClassAndInstance />,pageName:"クラスの定義とインスタンス"},
+        { pageID: 3, url: `/Collection` ,component:<Collection />,pageName:"コレクション"},
+        { pageID: 4, url: `/Overrideofmethod` ,component:<OverrideOfMethod />,pageName:"メソッドのオーバーライド"},
       ];
       return explanationData;
   };
--- src/components/explanationPage/ClassAndInstance.jsx
+++ src/components/explanationPage/ClassAndInstance.jsx
@@ -15,38 +15,128 @@ const Collection = () =>{
       <h3 className={explanationPagestyles.sectionTitle}>クラスの定義とインスタンス</h3>
       <h4 className={explanationPagestyles.paragraphTitle}>クラス宣言</h4>
       <div className={explanationPagestyles.paragrapContents}>
-        クラス呼び出しの引数
-        <h5 className={explanationPagestyles.itemTitle}>可変長引数</h5>
+        メソッドの引数
+        <h5 className={explanationPagestyles.itemTitle}>mainメソッドの引数</h5>
         <div className={explanationPagestyles.itemContents}>
-          型宣言でジェネリクスの型指定をしていれば、インスタンスの型指定は省略可能。<br />
+          <p>mainメソッドのみ、引数は以下のどちらかしか設定できない。</p>
+          <list>
+            <li>String型の配列</li> 
+            <li>String型の可変長引数</li> 
+          </list>
+
         </div>
         <div class="sampleFile">
           （例）  
             <div class="marginLeft10">
-              <div class="filename">エラーとならない例</div>
+              <div class="filename">型の例</div>
                 <div class="block">
-                型宣言、インスタンス可時のどちらも型指定した例
-                    <pre>
-                      <code>
-                        <span>ArrayList{`<`}Integer{`>`} listIntger = new ArrayList{`<`}Integer{`>`}();</span>
-                      </code>
-                    </pre>              
-                  インスタンス時の型指定を省略した例
-                    <pre>
-                      <code>
-                        <span>ArrayList{`<`}String{`>`} listString = new ArrayList{`<`}{`>`}();</span>
-                      </code>
-                    </pre>              
-                    宣言時に型指定しない例例
-                    <pre>
-                      <code>
-                        <span>ArrayList listObj = new ArrayList;</span>
-                      </code>
-                    </pre>              
+                mainメソッドの引数例（配列型）
+                  <pre>
+                    <code>
+                      <div className="otherCode">
+                        <span class="numberLow">public class Main {'{'}</span>
+                        <span class="numberLow">    public static void main(String[] args) {'{'}</span>
+                        <span class="numberLow">        for(String s:args)  {'{'}</span>
+                        <span class="numberLow">            System.out.println(s);</span>
+                        <span class="numberLow">        {'}'}</span>
+                        <span class="numberLow">    {'}'}</span>
+                        <span class="numberLow">{'}'}</span>
+                      </div>
+                    </code>
+                  </pre>              
+                  mainメソッドの引数例（可変長引数型）
+                  <pre>
+                    <code>
+                      <div className="otherCode">
+                        <span class="numberLow">public class Main {'{'}</span>
+                        <span class="numberLow">    public static void main(String... args) {'{'}</span>
+                        <span class="numberLow">        for(String s:args)  {'{'}</span>
+                        <span class="numberLow">            System.out.println(s);</span>
+                        <span class="numberLow">        {'}'}</span>
+                        <span class="numberLow">    {'}'}</span>
+                        <span class="numberLow">{'}'}</span>
+                      </div>
+                    </code>
+                  </pre>              
+                  mainメソッドの引数例（String型以外はコンパイルエラーとなる）
+                  <pre>
+                    <code>
+                      <div className="otherCode">
+                        <span class="numberLow">public class Main {'{'}</span>
+                        <span class="numberLow "><span className="fontred">&#10006;  public static void main(int[] args) {'{'}</span></span>
+                        <span class="numberLow">        for(int s:args)  {'{'}</span>
+                        <span class="numberLow">            System.out.println(s);</span>
+                        <span class="numberLow">        {'}'}</span>
+                        <span class="numberLow">    {'}'}</span>
+                        <span class="numberLow">{'}'}</span>
+                      </div>
+                    </code>
+                  </pre>              
+                  </div>
+
+                  
+            </div>
+        </div> 
+
+        <h5 className={explanationPagestyles.itemTitle}>main以外のメソッドの引数</h5>
+        <div className={explanationPagestyles.itemContents}>
+          <p>main以外のメソッドでは、引数は以下が設定できる。</p>
+          <list>
+          <li>型</li> 
+          <li>クラス</li> 
+          <li>型の配列</li> 
+          <li>型やクラスの可変長引数</li> 
+          </list>
+
+        </div>
+        <div class="sampleFile">
+          （例）  
+            <div class="marginLeft10">
+              <div class="filename">型の例</div>
+                <div class="block">
+                main以外のメソッドの引数例（int型の配列）
+                  <pre>
+                    <code>
+                      <div className="otherCode">
+                        <span class="numberLow">public class Main {'{'}</span>
+                        <span class="numberLow">    public static void main(String[] args) {'{'}</span>
+                        <span class="numberLow">        int[] intArray= {'{'}1, 2, 3{'}'};</span>
+                        <span class="numberLow">            new Main().printa(intArray);</span>
+                        <span class="numberLow">        {'}'}</span>
+                        <span class="numberLow">    </span>
+                        <span class="numberLow">        void printa(int[] args) {'{'}</span>
+                        <span class="numberLow">            for(int i:args) {'{'}</span>
+                        <span class="numberLow">            System.out.println(i);</span>
+                        <span class="numberLow">        {'}'}</span>
+                        <span class="numberLow">    {'}'}</span>
+                        <span class="numberLow">{'}'}</span>
+                      </div>
+                    </code>
+                  </pre>              
+                  main以外のメソッドの引数例（int型の可変長引数）
+                  <pre>
+                    <code>
+                      <div className="otherCode">
+                        <span class="numberLow">public class Main {'{'}</span>
+                        <span class="numberLow">    public static void main(String[] args) {'{'}</span>
+                        <span class="numberLow">        int[] intArray= {'{'}1, 2, 3{'}'};</span>
+                        <span class="numberLow">            new Main().printa(intArray);</span>
+                        <span class="numberLow">        {'}'}</span>
+                        <span class="numberLow">    </span>
+                        <span class="numberLow">        void printa(int... args) {'{'}</span>
+                        <span class="numberLow">            for(int i:args) {'{'}</span>
+                        <span class="numberLow">            System.out.println(i);</span>
+                        <span class="numberLow">        {'}'}</span>
+                        <span class="numberLow">    {'}'}</span>
+                        <span class="numberLow">{'}'}</span>
+                      </div>
+                    </code>
+                  </pre>              
                   </div>
             </div>
         </div> 
       </div>
+
     </div>
     </>
     
